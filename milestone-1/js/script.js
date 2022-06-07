118fe0bf33faeb6511d7f58a5727aebc871a810d
dbonafin <demianbonafina@gmail.com>
2022-06-07 14:14:02 +0200

    milestone 1 done

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,6 +1,21 @@
 // MILESTONE 1
-// Scrivi un programma che stampi i numeri da 1 a 100.
-// Per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// Stampare in console.
 
+// CALC NUMBERS FROM 1 TO 100
+for(let i = 1; i <= 100; i++) {
+    let fizzOrBuzz;
+
+    // PRINT FIZZ FOR THE MULTIPLES OF 3 / PRINT BUZZ FOR THE MULTIPLES OF 5
+    if (i % 3 === 0) {
+        fizzOrBuzz = 'fizz';
+    } else if (i % 5 === 0)  {
+        fizzOrBuzz = 'buzz';
+    } 
+
+    // PRINT FIZZBUZZ FOR THE MULTIPLES OF BOTH 3 AND 5
+    if (i % 15 === 0)  {
+        fizzOrBuzz = 'fizzbuzz';
+    }
+
+    // PRINT IN THE BROWSER CONSOLE THE RESULT
+    console.log(fizzOrBuzz, i);
+}
